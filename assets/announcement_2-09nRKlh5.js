bdd0d87602d2790d792bd12d934e41bba378158e
lelelemonade <szl921818@gmail.com>
2025-04-20 14:30:03 +0000

    Deploying to gh-pages from @ lelelemonade/lelelemonade.github.io@da1adf1bfe6146215b9b12afcefc617608867101 🚀

---
+++ assets/announcement_2-09nRKlh5.js
@@ -0,0 +1,10 @@
+const n=`---
+layout: post
+# title: A long announcement with details
+date: 2024-08-01 10:00:00-0400
+inline: ture
+related_posts: false
+---
+Land in Berlin, Germany.
+`;export{n as default};
+//# sourceMappingURL=announcement_2-09nRKlh5.js.map
---
+++ assets/announcement_4-BJ8pLGv-.js
@@ -0,0 +1,9 @@
+const n=`---
+layout: post
+date: 2025-02-01 08:40:00-0400
+inline: true
+related_posts: false
+---
+
+Successfully joined trivago`;export{n as default};
+//# sourceMappingURL=announcement_4-BJ8pLGv-.js.map
