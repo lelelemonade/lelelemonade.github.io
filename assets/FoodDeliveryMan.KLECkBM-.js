007a322dda4022bdb182e87e91d78eccdf48001b
lelelemonade <szl921818@gmail.com>
2025-05-01 19:44:07 +0000

    Deploying to gh-pages from @ lelelemonade/lelelemonade.github.io@9fe01b4a46feadc09bc01ff34b8f9b90aed3f605 🚀

---
+++ assets/FoodDeliveryMan.KLECkBM-.js
@@ -0,0 +1,11 @@
+const e=`---
+layout: post
+title: Started Food Delivery Position
+date: 2020-04-20 08:40:00-0400
+inline: true
+related_posts: false
+---
+
+Start delivering food in [HuaNan Seafood market](https://en.wikipedia.org/wiki/Huanan_Seafood_Wholesale_Market), Wuhan.
+I don't start coronavirus, don't blame me🤣.`;export{e as default};
+//# sourceMappingURL=FoodDeliveryMan.KLECkBM-.js.map
